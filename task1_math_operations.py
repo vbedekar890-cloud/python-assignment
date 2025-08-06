083dbaa18fc3d71a8afd4977f3a73b3fdd3c3102
vbedekar890-cloud <vbedekar890@gmail.com>
2025-08-06 07:02:33 +0530

    Add files via upload

---
+++ task1_math_operations.py
@@ -0,0 +1,15 @@
+num1 = 50
+num2 = 40
+
+# Performing operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# Displaying results
+print("Results:")
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
---
+++ task2_greeting.py
@@ -0,0 +1,10 @@
+# task2_greeting.py
+
+# Using hardcoded name as per your request
+first_name = "Sushmita"
+last_name = "Kapure"
+
+
+# Creating full name and greeting
+full_name = first_name + " " + last_name
+print(f"\nHello, {full_name}! Welcome to the world of Python.")
